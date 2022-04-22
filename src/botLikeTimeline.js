4e91dc59b7d50a3b7b7155d21aaec02b01f05792
Mas Okky <okkyalvianto@gmail.com>
2022-04-23 00:56:36 +0700

    fix botLikeTimeline.js

--- src/botLikeTimeline.js
+++ src/botLikeTimeline.js
@@ -1,96 +1,94 @@
-'use strict'
-
-const Client = require('instagram-private-api').V1;
-const delay = require('delay');
-const chalk = require('chalk');
-const _ = require('lodash');
-const inquirer = require('inquirer');
+"use strict";
+const { IgApiClient } = require("instagram-private-api");
+const delay = require("delay");
+const chalk = require("chalk");
+const _ = require("lodash");
+const inquirer = require("inquirer");
+const ig = new IgApiClient();
 
 const User = [
-    {
-        type:'input',
-        name:'username',
-        message:'[>] Insert Username:',
-	validate: function(value){
-		if(!value) return 'Can\'t Empty';
-		return true;
-	}
+  {
+    type: "input",
+    name: "username",
+    message: "[>] Insert Username:",
+    validate: function (value) {
+      if (!value) return "Can't Empty";
+      return true;
     },
-    {
-        type:'password',
-        name:'password',
-        message:'[>] Insert Password:',
-        mask:'*',
-	validate: function(value){
-		if(!value) return 'Can\'t Empty';
-		return true;
-	}
+  },
+  {
+    type: "password",
+    name: "password",
+    message: "[>] Insert Password:",
+    mask: "*",
+    validate: function (value) {
+      if (!value) return "Can't Empty";
+      return true;
     },
-    {
-        type:'input',
-        name:'sleep',
-        message:'[>] Insert Sleep (MiliSeconds):',
-        validate: function(value){
-            value = value.match(/[0-9]/);
-            if (value) return true;
-            return 'Delay is number';
-        }
+  },
+  {
+    type: "input",
+    name: "sleep",
+    message: "[>] Insert Sleep (Seconds):",
+    validate: function (value) {
+      value = value.match(/[0-9]/);
+      if (value) return true;
+      return "Delay is number";
+    },
+  },
+];
+
+const Login = async (User) => {
+  try {
+    ig.state.generateDevice(User.username);
+    const account = await ig.account.login(User.username, User.password);
+    return Promise.resolve({ account });
+  } catch (err) {
+    return Promise.reject(err);
+  }
+};
+
+const Like = async (media) => {
+  try {
+    if (media.has_liked) {
+      return chalk`{bold.blue Already Liked}`;
     }
-]
+    await ig.media.like({
+      mediaId: media.id,
+      moduleInfo: "feed_timeline",
+      d: 1,
+    });
+    return chalk`{bold.green Success Like}`;
+  } catch (err) {
+    return chalk`{bold.red Failed Like}`;
+  }
+};
 
-const Login = async function(User){
-
-    /** Save Account **/
-    const Device = new Client.Device(User.username);
-    const Storage = new Client.CookieMemoryStorage();
-    const session = new Client.Session(Device, Storage);
-
-    try {
-        await Client.Session.create(Device, Storage, User.username, User.password)
-        const account = await session.getAccount();
-        return Promise.resolve({session,account});
-    } catch (err) {
-        return Promise.reject(err);
-    }
-
-}
-
-const Like = async function(session,media){
-    try {
-        if (media.params.hasLiked) {
-           return chalk`{bold.blue Already Liked}`;
-        }
-        await Client.Like.create(session, media.id);
-        return chalk`{bold.green Success Like}`;
-    } catch (err) {
-        return chalk`{bold.red Failed Like}`;
-    }
-}
-
-const Excute = async function(User, sleep){
-    try {
-        console.log(chalk`\n{yellow [?] Try to Login . . .}`);
-        const doLogin = await Login(User);
-        console.log(chalk`{green [!] Login Succsess}, {yellow [?] Try Like All Media in Feed / Timeline . . .\n}`);
-        const feed = new Client.Feed.Timeline(doLogin.session);
-        var cursor;
-        do {
-            if (cursor) feed.setCursor(cursor);
-            var media = await feed.get(1);
-            media = _.chunk(media, 10);
-            for (var i = 0; i < media.length; i++) {
-                await Promise.all(media[i].map(async (media) => {
-                    const doLike = await Like(doLogin.session, media);
-                    console.log(chalk`[{bold.green Username:}] ${media.params.user.username}\n[{cyan ${media.id}}] => [${doLike}]`);
-                }))
-                await console.log(chalk`{yellow \n [#][>] Delay For ${sleep} MiliSeconds [<][#] \n}`);
-                await delay(sleep);
-            }
-        } while(feed.isMoreAvailable());
-    } catch (err) {
-        console.log(err);
-    }
-}
+const Excute = async (User, sleep) => {
+  try {
+    console.log(chalk`\n{yellow [?] Try to Login . . .}`);
+    await Login(User);
+    console.log(chalk`{green [!] Login Succsess}, {yellow [?] Try Like All Media in Feed / Timeline . . .\n}`);
+    const timelineFeed = ig.feed.timeline();
+    sleep = sleep * 1000;
+    do {
+      let medias = await timelineFeed.items();
+      medias = _.chunk(medias, 1);
+      for (let i = 0; i < medias.length; i++) {
+        await Promise.all(
+          medias[i].map(async (media) => {
+            const likeMedia = await Like(media);
+            console.log(chalk`[{bold.green Username:}] ${media.user.username}\n[{cyan ${media.id}}] => [${likeMedia}]`);
+            await console.log(chalk`{yellow \n [#][>] Delay For ${sleep / 1000} Seconds [<][#] \n}`);
+            await delay(sleep);
+          })
+        );
+      }
+    } while (timelineFeed.isMoreAvailable());
+  } catch (err) {
+    console.log(err);
+  }
+};
 
 console.log(chalk`
  {bold.cyan
@@ -98,18 +96,15 @@ console.log(chalk`
 
   [?] {bold.green Bot Like Timeline}
 
-  ——————————————————  [THANKS TO]  ————————————————————
-  [✓] CODE BY CYBER SCREAMER CCOCOT (ccocot@bc0de.net)
-  [✓] FIXING & TESTING BY SYNTAX (@officialputu_id)
-  [✓] CCOCOT.CO | BC0DE.NET | NAONLAH.NET | WingkoColi
-  [✓] SGB TEAM REBORN | Zerobyte.id | ccocot@bc0de.net 
-  —————————————————————————————————————————————————————}
+  ——————————————————  [+++++++++]  ————————————————————}
       `);
 
-inquirer.prompt(User)
-    .then(answers => {
-        Excute({
-            username:answers.username,
-            password:answers.password
-        },answers.sleep);
-    })
+inquirer.prompt(User).then((answers) => {
+  Excute(
+    {
+      username: answers.username,
+      password: answers.password,
+    },
+    answers.sleep
+  );
+});
